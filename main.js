0d2d2c90b8cf688cc9bc8f5bb00dc8e5b2f38c37
Paramrock <121217147+Paramrock@users.noreply.github.com>
2023-07-06 18:09:01 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+  clown_nose = loadImage('https://i.postimg.cc/MKrMZsjH/clown-nose.png');
+}
+
+function setup()
+{
+   canvas = createCanvas(300,300);
+   canvas.center();
+   video  = createCapture(VIDEO);
+   video.hide();
+   video.size(300,300)
+
+   poseNet = ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+   if(results.length > 0)
+   {
+     console.log(results);
+     noseX = results[0].pose.nose.x - 15;
+     noseY = results[0].pose.nose.y - 15;
+     console.log("nose x = " + noseX);
+     console.log("nose y = " + noseY);
+   }
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized')
+}
+
+function draw()
+{
+   image(video,0,0,300,300);
+   image(clown_nose,noseX,noseY,30,30);
+}
+
+function take_snapshot()
+{
+    save('param.png')
+}
+
